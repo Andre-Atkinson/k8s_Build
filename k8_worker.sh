d6224b9f79ac8271fca5756510f66a275d647d67
anjo-Lagrimas <74160430+anjo-Lagrimas@users.noreply.github.com>
2021-11-22 06:02:15 +0800

    cleaned up worker script

--- k8_worker.sh
+++ k8_worker.sh
@@ -25,18 +25,6 @@ modprobe br_netfilter
 # Apply sysctl params without reboot
 sysctl --system
 
-#Install the required packages, if needed we can request a specific version.
-VERSION=1.22.4-00
-apt-get  -y update 
-apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION helm containerd apt-transport-https ca-certificates curl nfs-common
-apt-mark hold kubelet kubeadm kubectl containerd 
-
-#configure containerd
-mkdir -p /etc/containerd
-containerd config default |  tee /etc/containerd/config.toml
-sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
-systemctl restart containerd
-
 #add Google Cloud Public Signing Key
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
@@ -46,6 +34,18 @@ echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https:/
 #Add Helm apt Repo
 curl https://baltocdn.com/helm/signing.asc |  apt-key add -
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" |  tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+#Install the required packages, if needed we can request a specific version.
+VERSION=1.22.4-00
+apt-get -y update 
+apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION helm containerd apt-transport-https ca-certificates curl nfs-common
+apt-mark hold kubelet kubeadm kubectl containerd 
+
+#configure containerd
+mkdir -p /etc/containerd
+containerd config default |  tee /etc/containerd/config.toml
+sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
 
 #Ensure both are set to start when the system starts up.
 systemctl enable kubelet.service
