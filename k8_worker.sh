8e2ef7aac1787b05421581a9b33568e474716f1e
anjo-Lagrimas <74160430+anjo-Lagrimas@users.noreply.github.com>
2021-11-21 16:34:18 +0800

    removed sudo from k8_worker.sh

--- k8_worker.sh
+++ k8_worker.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
+apt-get -y update
+apt-get -y upgrade
 
 #Disable swap
 swapoff -a
-sudo sed -i 's/\/swap.img/#\/swap.img/' /etc/fstab
+sed -i 's/\/swap.img/#\/swap.img/' /etc/fstab
 
 #containerd prereqs
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+cat <<EOF |  tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
 
 # Setup required sysctl params, these persist across reboots.
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+cat <<EOF |  tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
 # Apply sysctl params without reboot
-sudo sysctl --system
+sysctl --system
 
 #Install containerd
-sudo apt-get  -y update 
-sudo apt-get install -y containerd apt-transport-https ca-certificates curl
+apt-get  -y update 
+apt-get install -y containerd apt-transport-https ca-certificates curl
 
 #configure containerd
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
-sudo systemctl restart containerd
+mkdir -p /etc/containerd
+containerd config default |  tee /etc/containerd/config.toml
+sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
 
 #add Google Cloud Public Signing Key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 #Add Kubernetes apt Repo
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" |  tee /etc/apt/sources.list.d/kubernetes.list
 
 #Add Helm apt Repo
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+curl https://baltocdn.com/helm/signing.asc |  apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" |  tee /etc/apt/sources.list.d/helm-stable-debian.list
 
 #Install the required packages, if needed we can request a specific version.
-sudo apt-get  -y update 
+apt-get  -y update 
 VERSION=1.22.4-00
-sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION helm
-sudo apt-mark hold kubelet kubeadm kubectl containerd helm
+apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION helm
+apt-mark hold kubelet kubeadm kubectl containerd helm
 
 #Ensure both are set to start when the system starts up.
-sudo systemctl enable kubelet.service
-sudo systemctl enable containerd.service
-
+systemctl enable kubelet.service
+systemctl enable containerd.service
